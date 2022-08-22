8307307252f8ce2299b045004ca2b46f3ba173aa
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-08-22 22:52:02 +0900

    applied

--- mobiconv.py
+++ mobiconv.py
@@ -26,7 +26,7 @@ class MobiConvBlock(nn.Module):
             h = F.avg_pool2d(x, kernel_size=size, stride=size)
             h = conv(h)
             h = F.upsample(h, scale_factor=size, mode='nearest')
-            h = table * h + torch.logical_not(table) * conv.bias
+            h = table * h + torch.logical_not(table) * conv.bias.unsqueeze(0).unsqueeze(2).unsqueeze(3)
             threshold = self.ratio * torch.amax(h, dim=(-2, -1), keepdim=True)
             table = torch.ge(h, threshold)
             out.append(h)
