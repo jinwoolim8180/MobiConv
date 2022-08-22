846ffb29d909cfb6b27a4241e979149f7a781b93
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-08-22 22:36:43 +0900

    applied

--- models/cifar/vgg.py
+++ models/cifar/vgg.py
@@ -57,7 +57,10 @@ def make_layers(cfg, batch_norm=False):
     i = 1
     for v in cfg:
         if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            if i <= 2:
+                layers += [SmartPool2d(scale=2, mode='maxpool')]
+            else:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             i += 1
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
