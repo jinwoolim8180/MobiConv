cf8ae3c72f56e1d21857e2fe69067cddd102faae
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-02 00:50:57 +0900

    applied

--- models/cifar/alexnet.py
+++ models/cifar/alexnet.py
@@ -13,13 +13,13 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            MobiConvBlock(3, 64, kernel_size=11, stride=4, padding=5, n_layers=8, n_pools=1),
+            MobiConvBlock(3, 64, kernel_size=11, stride=4, padding=5, n_layers=8, n_pools=0),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            MobiConvBlock(64, 192, kernel_size=5, padding=2, n_layers=8, n_pools=2),
+            MobiConvBlock(64, 192, kernel_size=5, padding=2, n_layers=8, n_pools=0),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            MobiConvBlock(192, 384, kernel_size=3, padding=1, n_layers=8, n_pools=1),
+            MobiConvBlock(192, 384, kernel_size=3, padding=1, n_layers=8, n_pools=0),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
