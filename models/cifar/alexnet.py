ec20420b57e158b500a2a1d9a2f8accfe7b37430
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-02 00:27:32 +0900

    applied

--- models/cifar/alexnet.py
+++ models/cifar/alexnet.py
@@ -3,7 +3,7 @@ Without BN, the start learning rate should be 0.01
 (c) YANG, Wei 
 '''
 import torch.nn as nn
-
+from mobiconv import MobiConvBlock
 
 __all__ = ['alexnet']
 
@@ -13,13 +13,13 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
+            MobiConvBlock(3, 64, kernel_size=11, stride=4, padding=5, n_layers=8, n_pools=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2, n_layers=8, n_pools=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1, n_layers=8, n_pools=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
